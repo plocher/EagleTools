261a32f663691c704bd08979166c990ca2cbe147
John Plocher <john.plocher@gmail.com>
2019-06-16 11:02:25 -0700

    Combined makefile, doEagle, several Eagle ULPs and CAM files into a set of python libs and commands

---
+++ CAMtool/fab/CHMTfeeders.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+"""
+Inspired by Sparkfun's CHMT ULP tutorial
+(https://www.sparkfun.com/sparkx/blog/2591)
+
+This module supports writing stand alone scripts (see eagle2chmp.py, for example)
+that can be run from a Makefile without invoking eagle and running their ULP interactively
+The Google Doc format is expanded and reordered from the Sparkfun original
+    feel free to look at / borrow the one referenced here with the default key,
+    but you will need to use your own doc if you wish to modify things :-)
+
+  -John Plocher
+   July 2018
+   www.SPCoast.com
+
+"""
+from fab.SiteConfiguration import * # local config details
+
+import csv
+import urllib2
+
+
+SKIP       = -1
+NOTFOUND   = -2
+
+# csv file column names
+tapesize   = 'Tape Size'
+feedernum  = 'Feeder Number'
+partname   = 'Component'
+feederXoff = 'Feeder XOffset'
+feederYoff = 'Feeder YOffset'
+pickheight = 'PickHeight'
+pickdelay  = 'PickDelay'
+pullspeed  = 'PullSpeed'
+pulldist   = 'Pull Distance'
+placeheight= 'Place Height'
+placespeed = 'Place Speed'
+head       = 'Head'
+rotation   = 'Part Rotation'
+place      = 'Place Component'
+usevacuum  = 'Check Vacuum'
+usevision  = 'Use Vision'
+partsizeX  = 'Size X'
+partsizeY  = 'Size Y'
+centroidX  = 'Centroid Correction X'
+centroidY  = 'Centroid Correction Y'
+aliases    = 'Aliases'
+stocknotes = 'Stock Notes'
+inventory  = 'Inventory Notes'
+reference  = 'Used On'
+reference  = 'Used On'
+
+options    = 'Options'
+
+# csv file column order
+idx = {}
+idx[tapesize]    = 0
+idx[feedernum]   = 1
+idx[partname]    = 2
+idx[feederXoff]  = 3
+idx[feederYoff]  = 4
+idx[pickheight]  = 5
+idx[pickdelay]   = 6
+idx[pullspeed]   = 7
+idx[pulldist]    = 8
+idx[placeheight]  = 9
+idx[placespeed]  = 10
+idx[head]        = 11
+idx[rotation]    = 12
+idx[place]       = 13
+idx[usevacuum]   = 14
+idx[usevision]   = 15
+idx[partsizeX]   = 16
+idx[partsizeY]   = 17
+idx[centroidX]   = 18
+idx[centroidY]   = 19
+idx[aliases]     = 20
+idx[stocknotes]  = 21
+idx[inventory]   = 22
+idx[reference]   = 23
+
+
+# Workflow:
+#    downloadFeederFile(feederfilename, GDocSheetKey):
+#
+#       Download a local copy of the Feeder Sheet from Google Docs
+#       that contains a list of the feeders and parts loaded into them
+#       on your CHMT machine:  feeders.csv
+#
+#    loadFeeders(feederfile)
+#
+#       Open feeders.csv, parse contents, create data structures, expand part aliases
+#       feeder[feederName]  => { place, usevacuum, usevision }
+#       component[partName] => feederName
+#       Returns feeder dict and component dict
+#
+#    getFeederForComponent(partName, components)
+#
+#       get feederName (or SKIP) for a given part
+
+def loadFeeders(feederfilename):
+    feeders = {}
+    components = {}
+
+    with open(feederfilename, 'rb') as feedercsv:
+        feedereader = csv.reader(feedercsv)
+        for row in feedereader:
+            if row[idx[tapesize]] == tapesize:
+                continue
+            if row[idx[tapesize]] == "NoMount":
+                continue
+            if row[idx[tapesize]] == "Stop":
+                break
+            num = row[idx[feedernum]]
+            if num == "":
+                continue
+            me = {}
+            for k,v in idx.items():
+                me[k] = row[v]
+
+            me[place]       = True if me[place]     == "Y" else False
+            me[usevacuum]   = True if me[usevacuum] == "Y" else False
+            me[usevision]   = True if me[usevision] == "Y" else False
+            #  Status = 0b.0000.0ABC
+            #  A = 1 = Use Vision
+            #  A = 0 = No Vision
+            #  B = 1 = Use Vacuum Detection
+            #  B = 0 = No Vacuum Detection
+            #  C = 1 = Skip placement
+            #  C = 0 = Place this component
+            #  Example: 3 = no place, vac, no vis
+            opt = 0
+            if me[usevision]:        opt |= 0x04
+            if me[usevacuum]:        opt |= 0x02
+            if not me[place]:        opt |= 0x01
+            me[options] = opt
+
+            feeders[num] = me
+
+    for fidx in sorted(feeders.iterkeys()):
+        f = feeders[fidx]
+        name = f[partname].lower()
+        ts   = f[tapesize].lower()
+        if name == '':
+            continue
+        if ts == "":
+            continue
+        if ts == "no mount":
+            continue
+        if ts == "stop":
+            continue
+        components[name] = fidx
+        if f[aliases] == '':
+            continue
+
+        aliaslist = f[aliases].split(':')
+        for a in aliaslist:
+            if a.lower in components:
+                continue
+            components[a.lower()] = fidx
+        # print "Alias: {} =>  {}".format(f[partname], aliaslist)
+    return (feeders, components)
+
+
+
+def downloadFeederFile(feederfilename, key):
+    # Borrowed from SparkFun's CHMT ulp...
+    # The ID from a 'Anyone with the link can view' shared level spreadsheet
+    # This spreadsheet contains configurations for each different reel of components
+    if key is None or key == '':
+        key = spreadsheet_key
+
+    if feederfilename is None or feederfilename == '':
+        feederfilename = defaultfeederfile
+    print("Downloading feeder data from the Google ..."),
+
+    # This is the public spreadsheet that contains all our feeder data
+    # I'm too tired to use OAuth at the moment
+    url = 'https://docs.google.com/spreadsheet/ccc?key=' + key + '&output=csv'
+    response = urllib2.urlopen(url)
+    data = response.read()
+
+    print("Writing to {} ...".format(feederfilename)),
+
+    with open(feederfilename, "wb") as code:
+        code.write(data)
+
+    print("Done")
+
+
+def getFeederForComponent(partName, components):
+    pn = partName.lower()
+    if pn not in components:
+        return SKIP
+    return components[pn]
+
+
+1;
---
+++ CAMtool/fab/EAGLEboard.py
@@ -0,0 +1,224 @@
+
+from fab.SiteConfiguration import * # local config details
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import re
+from fab import CHMTfeeders
+
+"""
+sort based on alphanumeric order
+   LED1 LED2 ... LED9 LED10...
+"""
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+"""
+Read a .eaglerc file and extract the layer information (profile, id, color) from it
+Profile 0, 1 and 2 are for the black, white an color background pallets
+"""
+def getLayers(eaglercfile):
+    #                        Palette.0.0 = "0xFF000000"
+    pattern = re.compile(r"""Palette                # opening tag
+                         \.(?P<profile>\d)          # profile number
+                         \.(?P<item>.*?)            # item number
+                         \s*=\s*                    # whitespace = ...
+                         "0x(?P<color>[0-9A-Fa-f]+) # hex alpha + RGB color
+                         """, re.VERBOSE)
+    palettes = {}
+    with open(eaglercfile, 'rb') as rcfile:
+        for line in rcfile:
+            match = pattern.match(line)
+            if match:
+                profile = int(match.group("profile"))
+                item    = int(match.group("item"))
+                color   = match.group("color")
+                if profile not in palettes:
+                    palettes[profile] = {}
+                palettes[profile][item] = color
+    return palettes
+
+"""
+Load the EAGLE CAD XML board file, generating dict's of
+    * packages (name, smd/pth, x & y boundary)
+    * layers (number, color)
+    as well as the DOM (for further exploration by caller)
+"""
+def loadBoard(boardname, palettes):
+    packages = {}
+
+    # Open XML document using minidom parser
+    DOMTree = xml.dom.minidom.parse(boardname)
+    X = DOMTree.documentElement
+
+    for P in X.getElementsByTagName("package"):
+        me = {}
+        me["name"] = P.getAttribute("name")
+        me["smd"] = False   # default
+        smd = P.getElementsByTagName("smd")
+        if (smd):
+            me["smd"] = True
+        (me["xmin"], me["ymin"], me["xmax"], me["ymax"]) = getSymbolBounds(P,10000,10000,-10000,-10000)
+        packages[me["name"].lower()] = me
+        # print "Loaded: {} - bounds: ({},{}) ({},{})".format(me['name'],me["xmin"], me["ymin"], me["xmax"], me["ymax"])
+
+    # <layers>
+    # <layer number="1" name="Top" color="4" fill="1" visible="yes" active="yes"/>
+
+    layers = {}
+    for L in X.getElementsByTagName("layers"):
+        for l in L.getElementsByTagName("layer"):
+            lnum  = int(l.getAttribute("number"))
+            litem = int(l.getAttribute("color"))
+            #print("Layer[{}] = palettes[{}][{}] = {}".format(lnum, 0, litem, palettes[0][litem]))
+            layers[lnum] = palettes[1][litem]
+    return (X, packages, layers)
+
+
+def getSymbolBounds(E, xmin, ymin, xmax, ymax):
+    for wire in E.getElementsByTagName("wire"):
+        #if wire.getAttribute("layer") == "20":
+        (xmin, ymin, xmax, ymax) = getElementBounds(wire, xmin, ymin, xmax, ymax)
+    return (xmin, ymin, xmax, ymax)
+
+def getElementBounds(E, xmin,ymin,xmax,ymax):
+    wx1 = float(E.getAttribute("x1"))
+    wy1 = float(E.getAttribute("y1"))
+    wx2 = float(E.getAttribute("x2"))
+    wy2 = float(E.getAttribute("y2"))
+    if (wx1 > xmax): xmax = wx1
+    if (wy1 > ymax): ymax = wy1
+    if (wx1 < xmin): xmin = wx1
+    if (wy1 < ymin): ymin = wy1
+    if (wx2 > xmax): xmax = wx2
+    if (wy2 > ymax): ymax = wy2
+    if (wx2 < xmin): xmin = wx2
+    if (wy2 < ymin): ymin = wy2
+    return (xmin, ymin, xmax, ymax)
+
+"""
+My boards all have a special SYMBOL called "BOARD" that defines the DIM layer bounds.
+
+I generally do not use individual 'lines on the DIM layer, instead I have defined many 
+PACKAGES for the board SYMBOL, 
+    e.g., 5cmx5cm, 10x10, 10x10 with rounded corners, 10x10 rounded with mounting holes, etc.
+I can easily change the size of a board under design (change package...) if needed, and
+with a common set of board footprints, I know the sizes for PCB fabrication, and more 
+importantly, that all my jigs and test fixtures will work with the design.
+
+This routine looks for a BOARD, and if found, uses its dimensions.
+If one is not found, fall back to looking for wires on layer 20.
+otherwise, return 0
+
+"""
+def getBoardDimensions(eagleBoard):
+    xmax = -100000.0
+    xmin = 100000.0
+    ymax = xmax
+    ymin = xmin
+
+    for P in eagleBoard.getElementsByTagName("package"):
+        name = P.getAttribute("name")
+        if name.startswith('BOARD'):
+            (xmin, ymin, xmax, ymax) = getSymbolBounds(P, xmin, ymin, xmax, ymax)
+    if xmax > -100000.0:
+        return (xmin,ymin,xmax,ymax)
+
+    # no BOARD found, or no layer="20" wires on it, look for <board>'s layer20
+    for P in eagleBoard.getElementsByTagName("board"):
+        (xmin, ymin, xmax, ymax) = getSymbolBounds(P, xmin, ymin, xmax, ymax)
+
+    if xmax > -100000.0:
+        return (xmin, ymin, xmax, ymax)
+
+    # no dimensions found...
+    return (0,0,0,0)
+
+"""
+SMD Parts generally have no plated PADS
+Only SMD Parts are pick-n-placed automatically
+This returns a dict of SMD parts used in a design.
+"""
+def getSMDParts(eagleBoard, packages, component, feeder):
+    parts = {}
+
+    Boards = eagleBoard.getElementsByTagName("board")
+    for B in Boards:
+        # Get all the elements in the collection
+        E = B.getElementsByTagName("element")
+
+        # Print detail of each element.
+        for e in E:
+            if not e.hasAttribute("name"):
+                continue
+            # <element name="LED1" library="SPCoast" package="0603-LED" value="R" x="19.05" y="38.1" smashed="yes" rot="R180">
+
+            me = {}
+            me['name']    = e.getAttribute("name")
+            me['library'] = e.getAttribute("library")
+            me['package'] = e.getAttribute("package")
+            me['value']   = e.getAttribute("value")
+            me['x']       = e.getAttribute("x")
+            me['y']       = e.getAttribute("y")
+            me['rot']     = e.getAttribute("rot")
+            me['smashed'] = e.getAttribute("smashed")
+            me['smd']     = True  # corrected later if not...
+            me['feeder']  = CHMTfeeders.SKIP
+
+            id = me['value'].lower() + "-" + me['package'].lower()
+            me['id']      = id
+
+            parts[me['name']] = me
+
+            if (me['package'].lower() not in packages) or (not packages[me['package'].lower()]["smd"]):
+                # not a SMD part...
+                me['smd'] = False
+                continue
+
+            c = CHMTfeeders.getFeederForComponent(id, component)
+            if c != CHMTfeeders.SKIP:
+                me['feeder'] = c
+
+                # Normalize rotation...
+                # Most all Eagle FootPrints are correct but we have to subtract 90 because
+                # the CHMT tapes are mounted 90 degrees from the board
+                if me['rot'] == '':
+                    me['rot'] = 'R0'
+                if me['rot'].startswith('R'):
+                    angle = int(me['rot'].lstrip('R'))
+                else:
+                    angle = int(me['rot'])
+                angle = angle - 90
+
+                # However, some feeders/FPs are not horizontal (trays...)
+                # so we correct on a component by component basis
+                angle = angle + int(feeder[c][CHMTfeeders.rotation])
+                if (angle > 180):
+                    angle -= 360
+                me['rot'] = angle
+
+                #print "SMD: {name:<10} {id} feeder: {feed}".format(name= e.getAttribute("name"), id= id, feed=component[id])
+            else:
+                me['feeder'] = CHMTfeeders.NOTFOUND
+                # print "Note: Could not find feeder for part {} (ret={})".format(id, c)
+        return parts
+
+"""
+The feeder sheet can mark a component as "SKIP"
+If a design's part doesn't have an assigned feeder, assume it isn't used.
+"""
+def getUsedComponents(parts, feeder):
+    used = {}
+    for p in sorted(parts.iterkeys()):
+        if parts[p]['feeder'] == CHMTfeeders.SKIP:
+            continue
+        if parts[p]['feeder'] == CHMTfeeders.NOTFOUND:
+            continue
+        pn = feeder[parts[p]['feeder']][CHMTfeeders.partname]
+        if pn not in used:
+            used[pn] = []
+        used[pn].append(p)
+    return used
+
+1;
